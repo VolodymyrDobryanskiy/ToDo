1b7c756531d6283f185667adf32df4ff356d76fb
VolodymyrDobryanskiy <82653336+VolodymyrDobryanskiy@users.noreply.github.com>
2022-08-15 22:06:08 +0300

    Update theme.js
    Adding a dark theme

--- js/theme.js
+++ js/theme.js
@@ -1,15 +1,25 @@
 const checkbox = document.getElementById('checkbox');
 const switcher = document.querySelector('.switcher');
 const themesBody = document.querySelector('#body');
+const formColor = document.querySelector('#formColor');
+const formColorDark = document.querySelector('.form-control');
 
 
 switcher.addEventListener('click', () => {
-    checkbox.checked ?
-        switcher.setAttribute('data-themes', 'night') :
+    const allLi = document.querySelectorAll('li');
+    if (checkbox.checked) {
+        switcher.setAttribute('data-themes', 'night');
+        themesBody.classList.add('darkBody');
+        formColor.classList.add('darkLi');
+        formColor.classList.remove('bg-light');
+        allLi.forEach( (li) => li.classList.add('darkLi'));
+        formColorDark.classList.add('darkBody');
+    } else {
         switcher.setAttribute('data-themes', 'day');
-
-    checkbox.checked ?
-        themesBody.classList.add('darkBody') :
         themesBody.classList.remove('darkBody');
+        formColor.classList.remove('darkLi');
+        formColor.classList.add('bg-light');
+        allLi.forEach( (li) => li.classList.remove('darkLi'))
+        formColorDark.classList.remove('darkBody');
+    }
 })
-
